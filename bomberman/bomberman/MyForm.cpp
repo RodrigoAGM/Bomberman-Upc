0d4bf0db824641c70cbdc76a5f1e03c9357b7652
enzoftware <lizama.enzo@gmail.com>
2017-06-19 05:50:53 -0500

    windows forms created

---
+++ bomberman/bomberman/MyForm.cpp
@@ -0,0 +1,8 @@
+#include "MyForm.h"
+using namespace bomberman;
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::Run(gcnew MyForm());
+	return 0;
+}
---
+++ bomberman/bomberman/Player.h
@@ -0,0 +1,24 @@
+#include<string>
+using namespace std;
+enum COLORS
+{
+	RED, GREEN, BLUE, YELLOW, BLACK, WHITE
+};
+class Player
+{
+	string nickname;
+	COLORS color;
+public:
+	Player();
+	~Player();
+
+	void setNickname(string nickname) {
+		this->nickname = nickname;
+	}
+
+	string getNickname() {
+		return this->nickname;
+	}
+
+};
+
